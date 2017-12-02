134bc3c64515656cdb4fa99b114b52a9958aa022
li1 <malte@sandstede.com>
2017-12-02 13:26:39 +0100

    erst / zweit gewinner + follower

--- src/queries.js
+++ src/queries.js
@@ -24,6 +24,81 @@ export const zweitstimmensieger =
 'from maxproB m, bundeslaendergebnisse b, bundeslaender bb, parteien p',
 'where m.m = b.zweitstimmen',
 'and b.bundeslandid = bb.id',
-'and b.parteiid = p.id'].join('\n');
+'and b.parteiid = p.id',
+'and m.bid = b.bundeslandid'].join('\n');
 
+export const zweitstimmenFollower =
+['with zweiterproBland (bid, zw) as (',
+'    select b1.bundeslandid, b1.zweitstimmen from bundeslandergebnisse b1',
+'    where b1.legislaturperiodeid = 2017',
+'    and 1 = (select count(*) from bundeslandergebnisse b2',
+'                        where b2.legislaturperiodeid = 2017',
+'                        and b2.zweitstimmen > b1.zweitstimmen',
+'                        and b1.bundeslandid = b2.bundeslandid)',
+')',
+'',
+'select bb.name as land, p.name as partei',
+'from zweiterproBland zw, bundeslandergebnisse b, bundeslaender bb, parteien p',
+'where zw.zw = b.zweitstimmen',
+'and b.bundeslandid = bb.id',
+'and b.parteiid = p.id',
+'and zw.bid = b.bundeslandid'].join('\n');
+
+export const erststimmensieger =
+['with maxproB (bid, m) as (',
+'select bundeslandid, max(erststimmen) from bundeslandergebnisse',
+'where legislaturperiodeid = 2017',
+'group by bundeslandid )',
+'',
+'select bb.name as land, p.name as partei',
+'from maxproB m, bundeslandergebnisse b, bundeslaender bb, parteien p',
+'where m.m = b.erststimmen',
+'and b.bundeslandid = bb.id',
+'and b.parteiid = p.id',
+'and m.bid = b.bundeslandid',
+''].join('\n');
+
+export const erststimmenFollower =
+['with zweiterproBland (bid, zw) as (',
+'    select b1.bundeslandid, b1.erststimmen from bundeslandergebnisse b1',
+'    where b1.legislaturperiodeid = 2017',
+'    and 1 = (select count(*) from bundeslandergebnisse b2',
+'                        where b2.legislaturperiodeid = 2017',
+'                        and b2.erststimmen > b1.erststimmen',
+'                        and b1.bundeslandid = b2.bundeslandid)',
+')',
+'',
+'select bb.name as land, p.name as partei',
+'from zweiterproBland zw, bundeslandergebnisse b, bundeslaender bb, parteien p',
+'where zw.zw = b.erststimmen',
+'and b.bundeslandid = bb.id',
+'and b.parteiid = p.id',
+'and zw.bid = b.bundeslandid'].join('\n');
+
+// select , anzahlwaehler 1.00 / anzahlwahlberechtigte,
+// (select dk.kandidatid 
+// from wahlkreiserststimmenergebnisse werst, direktkandidaturen dk
+// where we.wahlkreisid = werst.wahlkreisid
+// and we.legislaturperiodeid = werst.legislaturperiodeid and
+// werst.wahlkreisid = dk.wahlkreisid and 
+// werst.kandidatid = dk.kandidatid and
+// not exists (select * 
+//                from wahlkreiserststimmenergebnisse wersto
+//                     where wersto.legislaturperiodeid = werst.legislaturperiodeid
+//                     and wersto.wahlkreisid = werst.wahlkreisid
+//                     and wersto.anz > werst.anz)) as sieger
+// from wahlkreisergebnisse we, wahlkreisewaehlerwahlberechtigte www
+// where we.legislaturperiodeid = www.legislaturperiodeid 
+// and we.wahlkreisid = www.wahlkreisid
+
+// [17:27]  
+// die anfrage liefert die wahlkreisergebnisse
+// [17:28]  
+// und evtl den kandidaten dazu- joinen damit man an namen und so kommt
+// [17:28]  
+// und parei
+// [17:28]  
+// partei
+// [17:28]  
+// aber kann ich auch machen
 
--- src/server.js
+++ src/server.js
@@ -24,28 +24,28 @@ app.get("/", async (req, res) => {
   res.send(rows)
 } );
 
-const parteien = ["DIE LINKE", "Sozialdemokratische Partei Deutschlands", "Freie Demokratische Partei"];
-const parteienIdents = ["Linke", "SPD", "FDP"] //_.map(parteien, partei => partei.split(' ').join(''));
-const parteienZip = _.zipWith(parteien, parteienIdents, (p, i) => [p, i]);
+// const parteien = ["DIE LINKE", "Sozialdemokratische Partei Deutschlands", "Freie Demokratische Partei"];
+// const parteienIdents = ["Linke", "SPD", "FDP"] //_.map(parteien, partei => partei.split(' ').join(''));
+// const parteienZip = _.zipWith(parteien, parteienIdents, (p, i) => [p, i]);
 
-const parteiergebnisseQuery = partei =>
-"select distinct wke.wahlkreisid wahlkreis, wke.anz " + partei + " from parteien p, " +
-"wahlkreiserststimmenergebnisse wke, kandidaten k, direktkandidaturen dk " + 
-"where wke.kandidatid = k.id and wke.legislaturperiodeid = '2017' and k.parteiid = p.id " + 
-"and p.name = $1 order by wahlkreis asc"
+// const parteiergebnisseQuery = partei =>
+// "select distinct wke.wahlkreisid wahlkreis, wke.anz " + partei + " from parteien p, " +
+// "wahlkreiserststimmenergebnisse wke, kandidaten k, direktkandidaturen dk " + 
+// "where wke.kandidatid = k.id and wke.legislaturperiodeid = '2017' and k.parteiid = p.id " + 
+// "and p.name = $1 order by wahlkreis asc"
 
 
-const group = (key, mapArr) => _.groupBy(mapArr, e => e[key]);
-const mergeMapsByValueOfKey = (key, mapArr) => _.map(group(key, mapArr),  e => _.merge(...e));
+// const group = (key, mapArr) => _.groupBy(mapArr, e => e[key]);
+// const mergeMapsByValueOfKey = (key, mapArr) => _.map(group(key, mapArr),  e => _.merge(...e));
 
-app.get("/parteiergebnisse", async (req, res) => {
-  const results = await Promise.all(parteienZip.map(partei => dbConnector.query(parteiergebnisseQuery(partei[1]), [partei[0]])))
-  .then(r => r.map(qr => (qr.rows))) //get rows
-  .then(r => _.flatten(r))
-  .then(r => mergeMapsByValueOfKey("wahlkreis", r))
+// app.get("/parteiergebnisse", async (req, res) => {
+//   const results = await Promise.all(parteienZip.map(partei => dbConnector.query(parteiergebnisseQuery(partei[1]), [partei[0]])))
+//   .then(r => r.map(qr => (qr.rows))) //get rows
+//   .then(r => _.flatten(r))
+//   .then(r => mergeMapsByValueOfKey("wahlkreis", r))
 
-  res.send(results);
-});
+//   res.send(results);
+// });
 
 const sitzverteilung = "select p.name as partei, sitze from SitzverteilungBundestagParteien sbp, parteien p " +
   "where legislaturperiodeid = '2017' and p.id = sbp.parteiid";
@@ -64,6 +64,22 @@ app.get("/zweitstimmensieger", async (req, res) => {
   const { rows } = await dbConnector.query(queries.zweitstimmensieger);
   res.send(rows);
 })
+
+app.get("/zweitstimmenFollower", async (req, res) => {
+  const { rows } = await dbConnector.query(queries.zweitstimmenFollower);
+  res.send(rows);
+})
+
+app.get("/erststimmensieger", async (req, res) => {
+  const { rows } = await dbConnector.query(queries.erststimmensieger);
+  res.send(rows);
+})
+
+app.get("/erststimmenFollower", async (req, res) => {
+  const { rows } = await dbConnector.query(queries.erststimmenFollower);
+  res.send(rows);
+})
+
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
